3ca024c66b19efbdf4caa6860241837d079b3a46
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2018-02-12 12:32:13 -0600

    hi

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require ('cors');
+
+const port = process.env.PORT || 3030;
+
+const server = express();
+server.use(bodyParser.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.send('Hello World from hi ${port}')
+});
+server.post('/payload', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+server.listen(port, err => {
+    if (err) console.error(err);
+    console.log(`Magic happening on ${port}`);
+});
+
